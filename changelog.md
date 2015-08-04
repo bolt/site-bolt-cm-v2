25f5e5519c7bb6ae48c104efae22a92c60e4c94b
Bob den Otter <bob@twokings.nl>
2015-08-04 10:04:12 +0200

    Updating bolt to 2.2.8

--- changelog.md
+++ changelog.md
@@ -3,12 +3,26 @@ Bolt 2.2.next
 
 Nothing yet.
 
+
+Bolt 2.2.8
+----------
+
+Released 2015-07-31. Notable changes:
+
+- Fixed: Ensure grouped taxonomies aren't wiped from listing pages when toggling the publication status. (see #3910)
+- Fixed: Timed entries will no longer switch to 'unpublished' after update to 2.2.7 (see #3899)
+- Fixed: "Notice: Array to string conversion in /..../src/Storage.php on line 1071" (See #3893)
+- Fixed: Avoid a missing array key from displaying a warning (Thanks Fabschurt)
+- Updated: `squizlabs/php_codesniffer` requirement to `~2.0` due to upstream changes.
+- Fixed: Send storage event in publishTimedRecords (see #3879)
+- Fixed: Memory leak / loop in "new content" (see #3883)
+
 Bolt 2.2.5
 ----------
 
 Released 2015-07-24. Notable changes:
 
- - Performance: Don't request users if we don't have too, and streamline `isAllowed()` functionality. (#3847)
+ - Performance: Don't request users if we don't have to, and streamline `isAllowed()` functionality. (#3847)
  - Fixed / security: If a user is not root, do not allow them to change the file extension on rename in UI. (Thanks to Tim Coen of Curesec GmbH for bringing this issue to our attention. See #3815)
  - Fixed: Layout issue in Chrome 44. Pretty sure it's a weird bug in Chrome. (#3856)
  - Changed: Update JS Markdown Options to match Parsedown for consistency. (#3820)
@@ -28,7 +42,7 @@ Released 2015-06-25. Notable changes:
 
  - Fixed: Logic preventing building of local extension autoloader (Thanks timcooper, see #3699)
  - Fixed: Clipboard paste issue with fileuploader (Thanks timcooper, see #3702)
- - Added: Now possibile to use the search feature for spcific contenttype(s) (Thanks sbani, see #3713)
+ - Added: Now possibile to use the search feature for specific contenttype(s) (Thanks sbani, see #3713)
  - Fixed: Wrong interpretation of max_upload_filesize / post_max_size (Thanks tvlooy, see #3732)
  - Fixed: Password reset "Error: Divide by zero" (see #3730)
 
--- src/Storage.php
+++ src/Storage.php
@@ -13,6 +13,7 @@ use Bolt\Translation\Translator as Trans;
 use Doctrine\DBAL\Connection as DoctrineConn;
 use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Doctrine\DBAL\Query\QueryBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use utilphp\util;
 
@@ -373,7 +374,7 @@ class Storage
         }
 
         // Update taxonomy and record relationships
-        $this->updateTaxonomy($contenttype, $content->values['id'], $content->taxonomy);
+        $this->updateTaxonomy($content, $content->values['id'], $content->taxonomy);
         $this->updateRelation($contenttype, $content->values['id'], $content->relation);
 
         // Dispatch post-save event
@@ -1056,30 +1057,7 @@ class Storage
             return;
         }
 
-        $this->checkedfortimed["publish-" . $contenttype['slug']] = true;
-        $tablename = $this->getContenttypeTablename($contenttype);
-
-        try {
-            // Check if there are any records that need publishing.
-            $stmt = $this->app['db']->executeQuery(
-                "SELECT id FROM $tablename WHERE status = 'timed' and datepublish < CURRENT_TIMESTAMP"
-            );
-
-            // If there's a result, we need to set these to 'publish'.
-            if ($stmt->fetch() !== false) {
-                // This is where we do black magic voodoo, because `datechanged` has the server
-                // time, which is not necessarily the same as `CURRENT_TIMESTAMP()`. Awesome!
-                $query = sprintf(
-                    "UPDATE %s SET status = 'published', datechanged = '%s' WHERE status = 'timed' and datepublish < CURRENT_TIMESTAMP",
-                    $tablename,
-                    date('Y-m-d H:i:s')
-                );
-                $this->app['db']->query($query);
-            }
-        } catch (DBALException $e) {
-            $message = "Timed publication of records for $contenttype failed: " . $e->getMessage();
-            $this->app['logger.system']->critical($message, array('event' => 'exception', 'exception' => $e));
-        }
+        $this->timedHandleRecords($contenttype['slug'], 'publish');
     }
 
     /**
@@ -1090,32 +1068,117 @@ class Storage
     public function depublishExpiredRecords($contenttype)
     {
         // We need to do this only once per contenttype, max.
-        if (isset($this->checkedfortimed["depublish-" . $contenttype['slug']])) {
+        if (isset($this->checkedfortimed['depublish-' . $contenttype['slug']])) {
             return;
         }
 
-        $this->checkedfortimed["depublish-" . $contenttype['slug']] = true;
-        $tablename = $this->getContenttypeTablename($contenttype);
+        $this->timedHandleRecords($contenttype['slug'], 'depublish');
+    }
+
+    /**
+     * Handle scheduled publishing/de-publishing of records.
+     *
+     * @param string $contenttypeSlug
+     * @param string $type
+     */
+    private function timedHandleRecords($contenttypeSlug, $type)
+    {
+        $this->checkedfortimed[$type . '-' . $contenttypeSlug] = true;
+        $tablename = $this->getContenttypeTablename($contenttypeSlug);
 
         try {
-
-            // Check if there are any records that need depublishing.
-            $stmt = $this->app['db']->executeQuery(
-                "SELECT id FROM $tablename WHERE status = 'published' and datedepublish <= CURRENT_TIMESTAMP and datedepublish > '1900-01-01 00:00:01' and datechanged < datedepublish"
-            );
-
-            // If there's a result, we need to set these to 'held'.
-            if ($stmt->fetch() !== false) {
-                $query = sprintf(
-                    "UPDATE %s SET status = 'held', datechanged = '%s' WHERE status = 'published' and datedepublish <= CURRENT_TIMESTAMP and datedepublish > '1900-01-01 00:00:01' and datechanged < datedepublish",
-                    $tablename,
-                    date('Y-m-d H:i:s')
-                );
-                $this->app['db']->query($query);
+            // Check for record that need to be published/de-published
+            $recordIds = $this->timedListRecords($contenttypeSlug, $type);
+            if ($recordIds === false) {
+                return;
             }
+
+            /** @var QueryBuilder $query */
+            $query = $this->app['db']->createQueryBuilder()
+                        ->update($tablename)
+                        ->set('status', ':newstatus')
+                        ->set('datechanged', ':datechanged')
+                        ->setParameter('datechanged', date('Y-m-d H:i:s'))
+            ;
+
+            $this->timedWhere($query, $type);
+
+            $query->execute();
         } catch (DBALException $e) {
-            $message = "Timed de-publication of records for $contenttype failed: " . $e->getMessage();
+            $message = "Timed $type of records for $contenttypeSlug failed: " . $e->getMessage();
             $this->app['logger.system']->critical($message, array('event' => 'exception', 'exception' => $e));
+
+            return;
+        }
+
+        // If there are no listeners for this event, we can safely return early.
+        if (! $this->app['dispatcher']->hasListeners("timed.$type")) {
+            return;
+        }
+
+        try {
+            foreach ($recordIds as $recordId) {
+                $content = $this->getContent("$contenttypeSlug/$recordId", array('hydrate' => false, 'returnsingle' => true));
+
+                $event = new StorageEvent($content, array('contenttype' => $contenttypeSlug, 'create' => false));
+                $this->app['dispatcher']->dispatch("timed.$type", $event);
+            }
+        } catch (\Exception $e) {
+            $this->app['logger.system']->critical("Dispatch handling failed for $contenttypeSlug.", array('event' => 'exception', 'exception' => $e));
+        }
+    }
+
+    /**
+     * Set the QueryBuilder where parameters.
+     *
+     * @param QueryBuilder $query
+     * @param string       $type
+     *
+     * @return array|false
+     */
+    private function timedListRecords($contenttypeSlug, $type)
+    {
+        $tablename = $this->getContenttypeTablename($contenttypeSlug);
+
+        /** @var QueryBuilder $query */
+        $query = $this->app['db']->createQueryBuilder()
+            ->select('id')
+            ->from($tablename)
+            ->set('status', ':newstatus')
+            ->set('datechanged', ':datechanged')
+            ->setParameter('datechanged', date('Y-m-d H:i:s'))
+        ;
+
+        $this->timedWhere($query, $type);
+
+        return $query->execute()->fetchAll();
+    }
+
+    /**
+     * Set the QueryBuilder where parameters.
+     *
+     * @param QueryBuilder $query
+     * @param string       $type
+     */
+    private function timedWhere(QueryBuilder $query, $type)
+    {
+        if ($type === 'publish') {
+            $query
+                ->where('status = :oldstatus')
+                ->andWhere('datepublish < CURRENT_TIMESTAMP')
+                ->setParameter('oldstatus', 'timed')
+                ->setParameter('newstatus', 'published')
+            ;
+        } else {
+            $query
+                ->where('status = :oldstatus')
+                ->andWhere('datedepublish <= CURRENT_TIMESTAMP')
+                ->andWhere('datedepublish > :zeroday')
+                ->andWhere('datechanged < datedepublish')
+                ->setParameter('oldstatus', 'published')
+                ->setParameter('newstatus', 'held')
+                ->setParameter('zeroday', '1900-01-01 00:00:01')
+            ;
         }
     }
 
@@ -1559,7 +1622,7 @@ class Storage
     /**
      * Run existence and perform publish/depublishes.
      *
-     * @param array<string> contenttypeslugs to check
+     * @param array ContentType slugs to check
      *
      * @return mixed false, if any table doesn't exist
      *               true, if all is fine
@@ -2292,15 +2355,44 @@ class Storage
     }
 
     /**
+     * Properly handle grouped taxonomy with ordering.
+     *
+     * @see https://github.com/bolt/bolt/issues/3908
+     *
+     * @param Content $content
+     * @param array   $taxonomy
+     *
+     * @return array
+     */
+    private function getIndexedTaxonomy($content, $taxonomy)
+    {
+        if (Arr::isIndexedArray($taxonomy)) {
+            return $taxonomy;
+        }
+
+        $ret = array();
+        foreach ($taxonomy as $key) {
+            if ($content->group !== null) {
+                $ret[] = $content->group['slug'] . '#' . $content->group['order'];
+            } else {
+                $ret[] = $key;
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
      * Update / insert taxonomy for a given content-unit.
      *
-     * @param string  $contenttype
+     * @param Content $content
      * @param integer $contentId
      * @param array   $taxonomy
      */
-    protected function updateTaxonomy($contenttype, $contentId, $taxonomy)
+    protected function updateTaxonomy($content, $contentId, $taxonomy)
     {
-        $tablename = $this->getTablename("taxonomy");
+        $contenttype = $content->contenttype;
+        $tablename = $this->getTablename('taxonomy');
         $configTaxonomies = $this->app['config']->get('taxonomy');
 
         // Make sure $contenttypeslug is a 'slug'
@@ -2316,10 +2408,9 @@ class Storage
         }
 
         foreach ($contenttype['taxonomy'] as $taxonomytype) {
-
             // Set 'newvalues to 'empty array' if not defined
             if (!empty($taxonomy[$taxonomytype])) {
-                $newslugs = $taxonomy[$taxonomytype];
+                $newslugs = $this->getIndexedTaxonomy($content, $taxonomy[$taxonomytype]);
             } else {
                 $newslugs = array();
             }
@@ -2418,7 +2509,9 @@ class Storage
                 $valuewithorder = $slug . "#" . $currentsortorder;
                 $slugkey = '/' . $configTaxonomies[$taxonomytype]['slug'] . '/' . $slug;
 
-                if (!in_array($slug, $newSlugsNormalised) && !in_array($valuewithorder, $newSlugsNormalised) && !array_key_exists($slugkey, $newSlugsNormalised)) {
+                if (!in_array($slug, $newSlugsNormalised)
+                    && !in_array($valuewithorder, $newSlugsNormalised)
+                    && !array_key_exists($slugkey, $newSlugsNormalised)) {
                     $this->app['db']->delete($tablename, array('id' => $id));
                 }
             }
